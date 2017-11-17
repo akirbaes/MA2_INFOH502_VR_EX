7299e6c14c9c45730eb06977b1745bc6aaf0f5c6
Akira <akirbaes@ulb.ac.be>
2017-11-17 17:35:42 +0100

    Partial notes of work of séance 2
    Doing the first page of exercises:
    main creates a triangle and shows it, using a shader to color it (see screenshot)
    notes on how to open the project - see the bitbucket repository to get the files

---
+++ main.cpp
@@ -0,0 +1,198 @@
+// Local Headers
+#include "glitter.hpp"
+
+// System Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <GL/gl.h>
+
+// Standard Headers
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+#include "Shader.hpp"
+#include "Camera.hpp"
+
+
+// Callbacks
+static bool keys[1024]; // is a key pressed or not ?
+						// External static callback
+						// Is called whenever a key is pressed/released via GLFW
+static void key_callback(GLFWwindow* window, int key, int /*scancode*/, int action, int /*mode*/);
+static void mouse_button_callback(GLFWwindow* /*window*/, int button, int action, int /*mods*/);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+void showFPS(void);
+float deltaTime = 0.0f;
+Camera cam = Camera(0, 0, 0, 0, 0, 0, 0, 0);
+Shader shade = Shader("normal2.vertex", "green.fragment");
+
+int main(int argc, char * argv[]) {
+
+
+    // Load GLFW and Create a Window
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    auto mWindow = glfwCreateWindow(mWidth, mHeight, "INFO-H-502 - Team XXX", nullptr, nullptr);
+
+    // Check for Valid Context
+    if (mWindow == nullptr) {
+        fprintf(stderr, "Failed to Create OpenGL Context");
+        return EXIT_FAILURE;
+    }
+
+    // Create Context and Load OpenGL Functions
+    glfwMakeContextCurrent(mWindow);
+    gladLoadGL();
+    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+
+	// Set the required callback functions
+	glfwSetKeyCallback(mWindow, key_callback);
+	glfwSetCursorPosCallback(mWindow, mouse_callback);
+	glfwSetMouseButtonCallback(mWindow, mouse_button_callback);
+	glfwSetScrollCallback(mWindow, scroll_callback);
+
+	// Change Viewport
+	int width, height;
+	glfwGetFramebufferSize(mWindow, &width, &height);
+	glViewport(0, 0, width, height);
+	
+//http://www.opengl-tutorial.org/fr/beginners-tutorials/tutorial-2-the-first-triangle/
+	GLuint VAO; // handler to our VAO and VBO object
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	GLfloat data[] = {
+		-1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		1.0f,  1.0f, 0.0f,
+	};
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+	
+//https://learnopengl.com/#!Getting-started/Hello-Triangle
+	shade.compile();
+
+
+    // Rendering Loop
+    while (glfwWindowShouldClose(mWindow) == false) {
+		showFPS();
+
+        // Background Fill Color
+        glClearColor(0.75f, 0.25f, 0.25f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, VAO);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		shade.use();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
+	
+
+		/*GLuint VBO;
+		glGenBuffers(1, &VBO); // ask OpenGL to create one object
+		glBindBuffer(GL_ARRAY_BUFFER, VBO); // notify OGL that the object is of VBO type
+											// send the data to the GPU’s memory
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);       // Unbind this object
+		*/
+
+		// Flip Buffers and Draw
+		glfwSwapBuffers(mWindow);
+
+        glfwPollEvents();
+    }
+
+
+
+	glfwTerminate();
+    return EXIT_SUCCESS;
+}
+
+void showFPS(void) {
+	static double lastTime = glfwGetTime();
+	static int nbFrames = 0;
+
+	// Measure speed
+	double currentTime = glfwGetTime();
+	deltaTime = currentTime - lastTime;
+	nbFrames++;
+	if (currentTime - lastTime >= 1.0) { // If last prinf() was more than 1 sec ago
+										 // printf and reset timer
+		std::cout << 1000.0 / double(nbFrames) << " ms/frame -> " << nbFrames << " frames/sec" << std::endl;
+		nbFrames = 0;
+		lastTime += 1.0;
+	}
+}
+
+
+static void key_callback(GLFWwindow* window, int key, int /*scancode*/, int action, int /*mode*/)
+{
+	if (action == GLFW_PRESS)
+		keys[key] = true;
+	else if (action == GLFW_RELEASE)
+		keys[key] = false;
+
+	if (keys[GLFW_KEY_ESCAPE])
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	// V-SYNC
+	if (keys[GLFW_KEY_U]) {
+		static bool vsync = true;
+		if (vsync) {
+			glfwSwapInterval(1);
+		}
+		else {
+			glfwSwapInterval(0);
+		}
+		vsync = !vsync;
+	}
+
+	if ((keys[GLFW_KEY_0] || keys[GLFW_KEY_KP_0])) {
+		std::cout << "You have pressed 0" << std::endl;
+	}
+
+}
+
+static void mouse_button_callback(GLFWwindow* /*window*/, int button, int action, int /*mods*/) {
+	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
+		keys[GLFW_MOUSE_BUTTON_RIGHT] = true;
+	else
+		keys[GLFW_MOUSE_BUTTON_RIGHT] = false;
+
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+		keys[GLFW_MOUSE_BUTTON_LEFT] = true;
+	else
+		keys[GLFW_MOUSE_BUTTON_LEFT] = false;
+
+	if (button == GLFW_MOUSE_BUTTON_MIDDLE && action == GLFW_PRESS)
+		keys[GLFW_MOUSE_BUTTON_MIDDLE] = true;
+	else
+		keys[GLFW_MOUSE_BUTTON_MIDDLE] = false;
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (keys[GLFW_MOUSE_BUTTON_RIGHT]) {
+		std::cout << "Mouse Position : (" << xpos << ", " << ypos << ")" << std::endl;
+	}
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	if (keys[GLFW_MOUSE_BUTTON_LEFT]) {
+		std::cout << "Mouse Offset : " << yoffset << std::endl;
+		cam.ProcessMouseMovement(xoffset,yoffset);
+	}
+}
+
